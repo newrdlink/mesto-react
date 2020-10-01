25ef6ae5be1f1e3cff88656d414430a51d75526a
Roman Didukh <newrdlink@yandex.ru>
2020-10-01 15:39:58 +0300

    after first review 5

---
+++ build/precache-manifest.89dc7282e24a7469b7c9e6f5f698f7d5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcec2852580ee3f8d4da8e7e82a2ddcb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46257eeee9f7a89720a0",
+    "url": "./static/css/main.c6dca061.chunk.css"
+  },
+  {
+    "revision": "7ed3167885cae8739839",
+    "url": "./static/js/2.063a77ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.063a77ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46257eeee9f7a89720a0",
+    "url": "./static/js/main.799a273e.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "./static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "./static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "./static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "./static/media/basket.9c3b03ae.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "./static/media/button-plus.bdfb8ebb.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "./static/media/edit-pen.58afbe73.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "./static/media/heart-active.9fdebb6b.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "./static/media/heart.6c74d9b5.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "./static/media/logo-header.855a8c98.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "./static/media/pen-avatar.34c46850.svg"
+  }
+]);
